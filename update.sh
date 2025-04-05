2ff1c845238081799b6d486e75efae3d90a08701
AldiiX <aldiix@email.cz>
2025-04-05 13:34:08 +0200

    FIX: dalsi pokus o opravu 63451

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git pull
 docker rm educhemlp -f
 docker build -t educhemlp .
-docker run -d --name educhemlp -p 8120:80 --restart always educhemlp
+docker run -d --add-host=host.docker.internal:host-gateway --name educhemlp -p 8120:80 --restart always educhemlp
